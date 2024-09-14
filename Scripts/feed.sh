0cb42be1c73a0af5a87fe0502b946953205e042c
xinjoo <xiaoxin394@outlook.com>
2024-09-14 22:16:12 +0800

    Update feed.sh

--- Scripts/feed.sh
+++ Scripts/feed.sh
@@ -4,15 +4,15 @@
 #使用方法：包名 项目地址（来自github地址的用户名及库名） 项目分支（项目Branch） pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名
 # 文件夹名称（禁止带_等符号） 项目地址 项目分支
 echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git" >> feeds.conf.default
-openclash vernesong/OpenClash dev pkg
-passwall xiaorouji/openwrt-passwall main
-ssr-plus fw876/helloworld master
+#openclash vernesong/OpenClash dev pkg
+#passwall xiaorouji/openwrt-passwall main
+#ssr-plus fw876/helloworld master
 
-advancedplus VIKINGYFY/luci-app-advancedplus main
-gecoosac lwb1978/openwrt-gecoosac main
-luci-app-tailscale asvow/luci-app-tailscale main
+#advancedplus VIKINGYFY/luci-app-advancedplus main
+#gecoosac lwb1978/openwrt-gecoosac main
+#luci-app-tailscale asvow/luci-app-tailscale main
 
-lucky gdy666/luci-app-lucky main
-istore linkease/istore main
-nas-packages linkease/nas-packages master
-nas-luci linkease/nas-packages-luci main
+#lucky gdy666/luci-app-lucky main
+#istore linkease/istore main
+#nas-packages linkease/nas-packages master
+#nas-luci linkease/nas-packages-luci main
