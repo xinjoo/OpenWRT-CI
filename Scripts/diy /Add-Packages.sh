4ad68a6fc58d20b7406621c532a2bbb50c6eeed1
xinjoo <xiaoxin394@outlook.com>
2024-09-14 20:45:03 +0800

    Update Add-Packages.sh

--- Scripts/diy /Add-Packages.sh
+++ Scripts/diy /Add-Packages.sh
@@ -3,7 +3,7 @@
 #自定义添加软件
 #使用方法：包名 项目地址（来自github地址的用户名及库名） 项目分支（项目Branch） pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名
 # 文件夹名称（禁止带_等符号） 项目地址 项目分支
-
+echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git" >> feeds.conf.default
 openclash vernesong/OpenClash dev pkg
 passwall xiaorouji/openwrt-passwall main
 ssr-plus fw876/helloworld master
