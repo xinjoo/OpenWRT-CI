5cd7bc5e7d05958582a0f064015a6f426aaab164
xinjoo <xiaoxin394@outlook.com>
2024-09-14 21:03:53 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -26,7 +26,8 @@ while IFS= read -r line; do
 
     # 调用 UPDATE_PACKAGE 函数
     UPDATE_PACKAGE $line
-done < $GITHUB_WORKSPACE/diy/Add-Packages.sh
+done < $GITHUB_WORKSPACE/feed.sh
+
 
 #UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
