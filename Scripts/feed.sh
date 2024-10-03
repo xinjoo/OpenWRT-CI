b5a74980766eea2db60c81916bdf1fefc7ae5ee1
xinjoo <xiaoxin394@outlook.com>
2024-10-04 00:41:37 +0800

    Update feed.sh

--- Scripts/feed.sh
+++ Scripts/feed.sh
@@ -3,7 +3,7 @@
 #自定义添加软件
 #使用方法：包名 项目地址（来自github地址的用户名及库名） 项目分支（项目Branch） pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名
 # 文件夹名称（禁止带_等符号） 项目地址 项目分支
-echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git" >> feeds.conf.default
+#echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git" >> feeds.conf.default
 #advancedplus VIKINGYFY/luci-app-advancedplus main
 #gecoosac lwb1978/openwrt-gecoosac main
 #luci-app-tailscale asvow/luci-app-tailscale main
